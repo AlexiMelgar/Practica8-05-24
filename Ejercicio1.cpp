aa84a0ab0c15644be5824dfb40c01ef9cf9192ad
AlexiMelgar <164561855+AlexiMelgar@users.noreply.github.com>
2024-05-08 12:47:52 -0600

    Add files via upload

---
+++ Ejercicio1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char opcion, variable='x';
+
+    do
+    {
+    cout <<"Menu"<<"\n";
+    cout <<"A Carne asada"<<"\n";
+    cout <<"B pollo"<<"\n";
+    cout <<"C cerdo"<<"\n";
+    cout <<"Seleccione una opcion:"<<"\n";
+    cin >> opcion;
+    cout <<"Opcion seleccionada "<<opcion<<"\n";
+
+    } while (opcion != 'C'&& opcion !='B'&& opcion !='A');
+
+    return 0;
+}
